b7029f2be5c35996fe0b2d38f760481927e11695
mglfranco <mglsantos96@gmail.com>
2026-02-25 01:23:08 -0300

    feat: Add PWA Manifest and ServiceWorker for Mobile App install

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('bookbot-store-v1').then((cache) => cache.addAll([
+            './',
+            './index.html',
+            './icon-192.png',
+            './icon-512.png',
+            './manifest.json'
+        ])),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
